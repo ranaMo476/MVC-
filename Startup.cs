84f85a31f79dd03cfc38e93c6a2ac018394b1507
ranaMo476 <78308759+ranaMo476@users.noreply.github.com>
2024-07-03 23:20:32 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,78 @@
+using AutoMapper;
+using BusinessLogicLayer.interfaces;
+using BusinessLogicLayer.Reprositres;
+using DataAccessLayer.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVCProject.MappingProfiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<MVCAPPDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefualtConnection"));
+            });
+            services.AddScoped<IDepartmentRepo, DepartmentReposetary>();
+            // services.AddDbContext<MVCAPPDBContext>();
+            services.AddScoped<IEmployeeRepocs, EmployeeReposetary>();
+              services.AddDbContext<MVCAPPDBContext>(options=>{
+                  /////options.useSqlServer("server = . ; Database = MVCApp; Trusted_connection =True");
+                  options.UseSqlServer(Configuration.GetConnectionString("DefualtConnection"));
+             },ServiceLifetime.Singleton); // important
+
+            services.AddAutoMapper(M => M.AddProfile(new EmployeeProfiles() ));
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
